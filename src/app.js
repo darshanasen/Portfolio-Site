437bb20903ac8f6f35f8143ca30cb0eb61768bb3
drshnsn <darsh.sen@gmail.com>
2017-03-28 09:57:31 -0400

    scss work

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 (function () {
+	$('h1').addClass('animated tada');
 	$('.menuBG').on('click', function() {
 		$(this).toggleClass('animate')
 		$('.hamburger').toggleClass('animate')
-		// $('.menuBG').toggleClass('animate');
 		$('.nav').toggleClass('display');
+	})
+	$('.navLink').on('click', function() {
+		$('.nav').toggleClass('display');
+		$('.hamburger').toggleClass('animate')
 	})
 })();
