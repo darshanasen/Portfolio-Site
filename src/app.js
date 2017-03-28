f0b56567a991d90e92d913e727a3f1515976887a
drshnsn <darsh.sen@gmail.com>
2017-03-27 23:37:13 -0400

    js + css changes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 (function () {
-	$('.menu').on('click', function() {
+	$('.menuBG').on('click', function() {
 		$(this).toggleClass('animate')
 		$('.hamburger').toggleClass('animate')
-		$('.menuBG').toggleClass('animate');
-		$('.nav').toggleClass('animate');
+		// $('.menuBG').toggleClass('animate');
+		$('.nav').toggleClass('display');
 	})
 })();
